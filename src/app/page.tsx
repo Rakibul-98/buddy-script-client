14a6961d4fca88dd0f553cf89c85264f80eae630
Rakibul-98 <rakibul.rupom2001@gmail.com>
2026-04-02 09:23:17 +0600

    Add: configured the shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
     <div>
       hello from next app
+      <Button className="underline" variant="outline">Button</Button>
     </div>
   );
 }
